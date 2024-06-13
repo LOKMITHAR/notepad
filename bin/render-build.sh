bdc56e454501391cc1c3aac6bfa6273027d49741
shreyasskrishna <shreyasskrishna.al21@bitsathy.ac.in>
2024-06-13 21:31:55 +0530

    this is commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-curl --create-dirs -o $HOME/.postgresql/root.crt -O https://cockroachlabs.cloud/cluster/5fa57cc4-dabc-42d4-a7f1-2f749cad8d0c/cert
+curl --create-dirs -o $HOME/.postgresql/root.crt -O https://cockroachlabs.cloud/clusters/5fa57cc4-dabc-42d4-a7f1-2f749cad8d0c/cert
 bundle exec rake db:migrate
